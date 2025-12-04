89eaaab61492fc184bb1f4852fa745d07e3e9267
Max Burenko <maxburenko@gmail.com>
2025-12-04 13:30:15 +0600

    feat: add comment model

---
+++ src/models/comment.model.ts
@@ -0,0 +1,25 @@
+import { BaseModel } from '../objection/base.model';
+import { User } from './user.model';
+import { Card } from './card.model';
+
+export class Comment extends BaseModel {
+    static tableName = 'comments';
+
+    id!: number;
+    text!: string;
+    card_id!: number;
+    user_id!: number;
+
+    static relationMappings = {
+        user: {
+            relation: BaseModel.BelongsToOneRelation,
+            modelClass: User,
+            join: { from: 'comments.user_id', to: 'users.id' },
+        },
+        card: {
+            relation: BaseModel.BelongsToOneRelation,
+            modelClass: Card,
+            join: { from: 'comments.card_id', to: 'cards.id' },
+        },
+    };
+}
