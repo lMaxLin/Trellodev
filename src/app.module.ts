481cf7b4695898044103ca0bc858421bc43b65cc
Max Burenko <maxburenko@gmail.com>
2025-12-04 13:22:23 +0600

    refactor(app): import additional feature modules and reorganize AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+import { ObjectionModule } from './objection/objection.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ColumnsModule } from './columns/columns.module';
+import { CardsModule } from './cards/cards.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [],
+  imports: [
+      ObjectionModule,
+      AuthModule,
+      UsersModule,
+      ColumnsModule,
+      CardsModule,
+      CommentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
