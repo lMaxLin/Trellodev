f1b2b8ab0fbc449ddc86e0452a0f6731efd9e73e
Max Burenko <maxburenko@gmail.com>
2025-12-04 13:30:25 +0600

    feat: add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,31 @@
+import { BaseModel } from '../objection/base.model';
+import { Column } from './column.model';
+import { Card } from './card.model';
+import { Comment } from './comment.model';
+
+export class User extends BaseModel {
+    static tableName = 'users';
+
+    id!: number;
+    email!: string;
+    password!: string;
+    name?: string;
+
+    static relationMappings = {
+        columns: {
+            relation: BaseModel.HasManyRelation,
+            modelClass: Column,
+            join: { from: 'users.id', to: 'columns.user_id' },
+        },
+        cards: {
+            relation: BaseModel.HasManyRelation,
+            modelClass: Card,
+            join: { from: 'users.id', to: 'cards.user_id' },
+        },
+        comments: {
+            relation: BaseModel.HasManyRelation,
+            modelClass: Comment,
+            join: { from: 'users.id', to: 'comments.user_id' },
+        },
+    };
+}
