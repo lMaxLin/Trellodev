ade12672e67f8bee844b0833505e075426bc125c
Max Burenko <maxburenko@gmail.com>
2025-12-22 16:05:36 +0600

    feat: enhance app bootstrap with validation and Swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true , transform: true}));
+
+const  config = new DocumentBuilder()
+    .setTitle('Trello')
+    .setDescription('Trello-like API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+const doc = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, doc);
+
+await app.listen(process.env.PORT || 3000);
+console.log('Listening on', process.env.PORT || 3000);
 }
 bootstrap();
