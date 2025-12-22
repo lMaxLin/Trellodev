b93af1baece4bce5474bd89e99a3476fd674bde7
Max Burenko <maxburenko@gmail.com>
2025-12-22 15:57:21 +0600

    feat: implement user, auth, cards, columns, and comments modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,18 @@ import { Module } from '@nestjs/common';
 import { ObjectionModule } from './objection/objection.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ColumnsModule } from './columns/columns.module';
-import { CardsModule } from './cards/cards.module';
-import { CommentsModule } from './comments/comments.module';
+// import { ColumnsModule } from './columns/columns.module';
+// import { CardsModule } from './cards/cards.module';
+// import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
       ObjectionModule,
       AuthModule,
       UsersModule,
-      ColumnsModule,
-      CardsModule,
-      CommentsModule,
+      // ColumnsModule,
+      // CardsModule,
+      // CommentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
