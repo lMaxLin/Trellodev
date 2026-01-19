b2bd6b286692c2e4b07a26b713e48bbc0cdc79ff
Max Burenko <maxburenko@gmail.com>
2026-01-19 12:48:35 +0600

    feat: add ConfigService

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,19 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor() {
+    constructor( private config: ConfigService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: process.env.JWT_SECRET || 'secret',
+            secretOrKey: config.get<string>('JWT_SECRET')
         });
     }
-    async validate(payload: any) {
-        return { userId: payload.sub };
+    async validate(payload: { sub: number }) {
+        return { id: payload.sub };
     }
 }
