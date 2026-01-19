1dd7b969641428135eaf520f1ca82fa144af4a21
Max Burenko <maxburenko@gmail.com>
2026-01-19 12:56:22 +0600

    feat: forbid NonWhiteListed request properties

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ dotenv.config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.useGlobalPipes(new ValidationPipe({ whitelist: true , transform: true}));
+    app.useGlobalPipes(new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true
+    }));
 
 const  config = new DocumentBuilder()
     .setTitle('Trello')
